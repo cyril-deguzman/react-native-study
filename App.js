6857010d75a9945950d9a29ee13f265af2f6789f
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2021-11-17 20:27:00 +0800

    image and text study

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Pressable} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // handle<functionName> : convention
+    const handlePress = () => console.log("Text pressed");
+
+    // use console.log for quick debugs
+    console.log("start");
+
+    /*
+        - {{ width: 200, height: 300, uri: https://picsum.photos/200/300 }} third party assets
+        + <Image style={styles.tinyLogo} source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}/> third party assets
+    */ 
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>buset</Text>  
+
+            {/*load local assets*/}
+            <Image source={require('./assets/favicon.png')}/>
+
+            {/**
+             * onPress={()=> console.log("pressed")} longer version.
+             * Pressable tag to handle touch based input 
+             */}
+            <Pressable onPress={handlePress}>
+                <Image style={styles.tinyLogo} 
+                    source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+                />
+            </Pressable>
+            
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
 });
